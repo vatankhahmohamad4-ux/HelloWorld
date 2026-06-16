6f4ac90ce94e55bee4ca1635d297b8aa7639238b
Mohammad_Vatankhah <vatankhahmohamad4@gmail.com>
2026-06-16 17:53:15 +0330

    update

--- Core.py
+++ Core.py
@@ -1,6 +1,5 @@
-cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-cv_results = {}
 
 print("\n" + "=" * 60)
 print("  5-Fold Stratified Cross-Validation Results")
-print("=" * 60)
+
+list = [3,1]
